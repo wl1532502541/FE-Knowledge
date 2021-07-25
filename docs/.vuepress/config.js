f47bff1c62e0454ad39c6c8bf2c33d2c6e3711a5
wl1532502541 <1532502541@qq.com>
2021-07-25 21:07:45 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,15 @@ module.exports = {
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
-
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.png',
   },
+  plugins: [
+    [
+      'vuepress-plugin-yuque', {
+        // 这里可以使用你自己的语雀 Repo
+        repoUrl: 'https://www.yuque.com/wanglei-7ljle/ald1g0',
+      }
+    ]
+  ]
 }
